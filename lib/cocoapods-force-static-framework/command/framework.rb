287cd998a89dbbc62568501bb6dceb202ad42102
maqiang <qiang.ma@nio.com>
2019-10-12 15:42:17 +0800

    Update

--- lib/cocoapods-force-static-framework/command/framework.rb
+++ lib/cocoapods-force-static-framework/command/framework.rb
@@ -16,15 +16,15 @@ module Pod
           should_static = options.delete(Pod::Static.keyword)
           requirements.pop if options.empty?
           pod_name = Specification.root_name(name)
-          @force_stati_framework_names ||= []
-          @force_stati_framework_names.push pod_name
+          @force_static_framework_names ||= []
+          @force_static_framework_names.push pod_name
         end
       end
 
-      def force_stati_framework_names
-        names = @force_stati_framework_names || []
+      def force_static_framework_names
+        names = @force_static_framework_names || []
         if parent != nil and parent.kind_of? TargetDefinition
-          names += parent.force_stati_framework_names
+            names += parent.force_static_framework_names
         end
         names
       end
@@ -44,8 +44,8 @@ module Pod
     old_method = instance_method(:initialize)
 
     define_method(:initialize) do |sandbox, host_requires_frameworks, user_build_configurations, archs, platform, specs, target_definitions, file_accessors, scope_suffix, build_type|
-      bt = Target::BuildType.infer_from_spec(specs.first, :host_requires_frameworks => host_requires_frameworks)
-      if target_definitions.first.force_stati_framework_names.include?(specs.first.name)
+      bt = build_type[:build_type]
+      if target_definitions.first.force_static_framework_names.include?(specs.first.name)
         bt = Target::BuildType.static_framework
       end
       old_method.bind(self).(sandbox, host_requires_frameworks, user_build_configurations, archs, platform, specs, target_definitions, file_accessors, scope_suffix, :build_type => bt)
--- lib/cocoapods-force-static-framework/gem_version.rb
+++ lib/cocoapods-force-static-framework/gem_version.rb
@@ -1,3 +1,3 @@
 module CocoapodsForceStaticFramework
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
